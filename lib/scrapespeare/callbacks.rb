54c9955b72a6b2a4247dc0cab6c2a4888645e1d8
Dominic Bauer <bauerdominic@gmail.com>
2014-11-15 19:32:19 +0100

    Add documentation for callbacks

--- lib/scrapespeare/callbacks.rb
+++ lib/scrapespeare/callbacks.rb
@@ -1,14 +1,23 @@
 module Scrapespeare
   module Callbacks
 
+    # @return [Hash]
     def callbacks
       @callbacks ||= {}
     end
 
+    # Registers a `Proc` as a callback
+    #
+    # @param context [Symbol]
+    # @param proc [Proc]
     def register_callback(context, &proc)
       (callbacks[context] ||= []) << proc
     end
 
+    # Calls all callbacks identified by `context` and passes `*argv`
+    #
+    # @param context [Symbol]
+    # @param argv [Array<Object>]
     def execute_callbacks(context, *argv)
       callbacks[context].each { |callback| callback.call(*argv) }
     end
