5020e9ccaf98b3889ab155e069b459b09e8353fd
Dominic Bauer <bauerdominic@gmail.com>
2014-11-17 14:08:41 +0100

    Specs pass

--- lib/scrapespeare/extractor_group.rb
+++ lib/scrapespeare/extractor_group.rb
@@ -1,5 +1,12 @@
 module Scrapespeare
   class ExtractorGroup
 
+    attr_reader :identifier
+
+    def initialize(identifier, &proc)
+      @identifier = identifier
+      instance_eval(&proc) if block_given?
+    end
+
   end
 end
--- spec/extractor_group_spec.rb
+++ spec/extractor_group_spec.rb
@@ -3,5 +3,25 @@ require "spec_helpers"
 module Scrapespeare
   describe ExtractorGroup do
 
+    describe "#initialize" do
+      let(:extractor_group) do
+        Scrapespeare::ExtractorGroup.new(:foobar)
+      end
+
+      it "sets @identifier" do
+        expect(extractor_group.identifier).to be :foobar
+      end
+
+      it "evaluates the given block in its instance context" do
+        context = nil
+
+        extractor_group = Scrapespeare::ExtractorGroup.new(:foobar) do
+          context = self
+        end
+
+        expect(context).to be extractor_group
+      end
+    end
+
   end
 end
