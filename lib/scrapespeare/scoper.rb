f856784b5e3e2b21e8ffbc71784982ab004dd015
Dominic Bauer <bauerdominic@gmail.com>
2014-11-17 16:27:07 +0100

    Spec passes

--- lib/scrapespeare/scoper.rb
+++ lib/scrapespeare/scoper.rb
@@ -1,5 +1,11 @@
 module Scrapespeare
   class Scoper
 
+    attr_reader :matcher
+
+    def initialize(matcher, &proc)
+      @matcher = Matcher.new(matcher)
+    end
+
   end
 end
--- spec/scoper_spec.rb
+++ spec/scoper_spec.rb
@@ -3,5 +3,17 @@ require "spec_helpers"
 module Scrapespeare
   class Scoper
 
+    describe "#initialize" do
+      let(:scoper) do
+        Scoper.new(css: ".foo")
+      end
+
+      it "sets @matcher" do
+        matcher = scoper.matcher
+        expect(matcher.type).to be :css
+        expect(matcher.expression).to eq ".foo"
+      end
+    end
+
   end
 end
