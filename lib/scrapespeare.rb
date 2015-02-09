3fe698455090ab86250b7a0acbf7268a07d18c94
Dominic Bauer <bauerdominic@gmail.com>
2015-02-09 18:35:46 +0100

    rm CLI

--- lib/scrapespeare.rb
+++ lib/scrapespeare.rb
@@ -18,7 +18,6 @@ require "scrapespeare/extractable_group"
 require "scrapespeare/extractor"
 require "scrapespeare/scraper"
 require "scrapespeare/crawler"
-require "scrapespeare/cli"
 
 module Scrapespeare
   extend Configurable
