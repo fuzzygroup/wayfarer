acc26d9718b12f2377c9dbc0b84e7043d861b65b
Dominic Bauer <bauerdominic@gmail.com>
2015-02-14 19:24:39 +0100

    werks

--- lib/scrapespeare/http_adapters/faraday_adapter.rb
+++ lib/scrapespeare/http_adapters/faraday_adapter.rb
@@ -2,10 +2,15 @@ module Scrapespeare
   module HTTPAdapters
     class FaradayAdapter
 
+      def initialize
+        @client = Faraday.new
+      end
+
       # Fires a HTTP GET request against the URI and returns the response body
       #
       # @param uri [String]
       def fetch(uri)
+        @client.get(uri).body
       end
 
     end
--- spec/http_adapters/faraday_adapter_spec.rb
+++ spec/http_adapters/faraday_adapter_spec.rb
@@ -6,6 +6,16 @@ module Scrapespeare
 
       let(:adapter) { FaradayAdapter.new }
 
+      describe "#fetch" do
+        before do
+          stub_request(:get, "http://example.com").to_return(body: "Succeeded")
+        end
+
+        it "returns the response body" do
+          expect(adapter.fetch("http://example.com")).to eq "Succeeded"
+        end
+      end
+
     end
   end
 end
