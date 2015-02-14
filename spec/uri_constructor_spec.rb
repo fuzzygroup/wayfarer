ef0717d340276d9de87230938dfdf6496ee30b9e
Dominic Bauer <bauerdominic@gmail.com>
2015-02-14 16:23:46 +0100

    werks

--- spec/uri_constructor_spec.rb
+++ spec/uri_constructor_spec.rb
@@ -6,13 +6,32 @@ module Scrapespeare
     let(:constructor) { URIConstructor }
 
     describe ".construct" do
-      let(:base_uri) { URI("http://example.com") }
-
       it "returns an URI" do
+        base_uri = URI("http://example.com")
         returned = constructor.construct(base_uri, "page_2")
         expect(returned.to_s).to eq "http://example.com/page_2"
+      end
+
+      it "returns an URI" do
+        base_uri = URI("http://example.com")
+        returned = constructor.construct(base_uri, "page_2&foo=bar")
+        expect(returned.to_s).to eq "http://example.com/page_2&foo=bar"
+      end
+
+      it "returns an URI" do
+        base_uri = URI("http://example.com/alpha/beta")
+        returned = constructor.construct(base_uri, "/gamma")
+        expect(returned.to_s).to eq "http://example.com/gamma"
+      end
+
+      it "returns an URI" do
+        base_uri = URI("http://example.com/alpha/beta")
+        returned = constructor.construct(base_uri, "/gamma?foo=bar")
+        expect(returned.to_s).to eq "http://example.com/gamma?foo=bar"
       end
     end
 
   end
 end
+
+# /lieferservices-munchen/81479/?doors=openorclosed&sort_by=newest&page=all
