1d224f56e392c9801198cbe80df1a8517a223821
Dominic Bauer <bauerdominic@gmail.com>
2015-02-19 23:38:44 +0100

    progress

--- lib/scrapespeare/paginator.rb
+++ lib/scrapespeare/paginator.rb
@@ -1,10 +1,35 @@
 module Scrapespeare
   class Paginator
 
-    def initialize(scraper, http_adapter, parser)
+    attr_reader :http_adapter
+    attr_reader :parser
+
+    def initialize(http_adapter, parser)
+      @http_adapter = http_adapter
+      @parser = parser
+
+      @state = {}
     end
 
-    def paginate
+    def paginate(uri)
+      @state[:uri] = uri
+
+      loop do
+        response_body = @http_adapter.fetch(uri)
+        doc = @parser.parse(response_body)
+
+        yield doc
+
+        break unless has_successor?
+      end
+    end
+
+    private
+    def has_successor?
+      @state[:uri] = successor_uri if successor_uri
+    end
+
+    def successor_uri
     end
 
   end
--- spec/paginator_spec.rb
+++ spec/paginator_spec.rb
@@ -3,7 +3,19 @@ require "spec_helpers"
 module Scrapespeare
   describe Paginator do
 
-    let(:paginator) { Paginator.new }
+    let(:paginator) do
+      Paginator.new(HTTPAdapters::RestClientAdapter.new, Parser)
+    end
+
+    describe "#initialize" do
+      it "sets @http_adapter" do
+        expect(paginator.http_adapter).not_to be nil
+      end
+
+      it "sets @parser" do
+        expect(paginator.parser).not_to be nil
+      end
+    end
 
   end
 end
