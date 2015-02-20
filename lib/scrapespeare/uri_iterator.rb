6baa6a96b9d9d069938b485a001e0eb27eba7a64
Dominic Bauer <bauerdominic@gmail.com>
2015-02-20 16:25:38 +0100

    werks

--- lib/scrapespeare/uri_iterator.rb
+++ lib/scrapespeare/uri_iterator.rb
@@ -1,5 +1,11 @@
 module Scrapespeare
   class URIIterator
-    
+
+    attr_reader :base_uri
+
+    def initialize(base_uri)
+      @base_uri = base_uri
+    end
+
   end
 end
--- spec/uri_iterator_spec.rb
+++ spec/uri_iterator_spec.rb
@@ -3,7 +3,13 @@ require "spec_helpers"
 module Scrapespeare
   describe URIIterator do
 
-    let(:iterator) { URIIterator.new }
+    describe "#initialize" do
+      let(:iterator) { URIIterator.new("http://example.com") }
+
+      it "sets @base_uri" do
+        expect(iterator.base_uri).to eq "http://example.com"
+      end
+    end
 
   end
 end
