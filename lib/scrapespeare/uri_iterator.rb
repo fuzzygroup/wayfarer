b2a1417779b5c96285f45e40a792ea5b03e74584
Dominic Bauer <bauerdominic@gmail.com>
2015-02-21 17:38:01 +0100

    reset

--- lib/scrapespeare/uri_iterator.rb
+++ lib/scrapespeare/uri_iterator.rb
@@ -12,21 +12,6 @@ module Scrapespeare
     end
 
     def each
-      param = @opts[:param]
-      upper_bound = @opts[:to]
-      step = @opts[:step] || 1
-
-      if upper_bound
-        (upper_bound + 1).times do
-          yield @uri
-          @uri.increment_query_param(param, step)
-        end
-      else
-        loop do
-          yield @uri
-          @uri.increment_query_param(param, step)
-        end
-      end
     end
 
   end
--- spec/uri_iterator_spec.rb
+++ spec/uri_iterator_spec.rb
@@ -20,42 +20,6 @@ module Scrapespeare
 
       describe "Query parameter iteration" do
         let(:opts) { { param: "page" } }
-
-        it "yields the expected URIs" do
-          enum = iterator.to_enum
-          expect(enum.next.to_s).to eq "http://example.com"
-          expect(enum.next.to_s).to eq "http://example.com?page=2"
-        end
-
-        context "with upper bound given" do
-          let(:opts) { { param: "page", to: 3 } }
-
-          it "yields the expected number of URIs" do
-            yielded = []
-
-            iterator.each do |uri|
-              yielded << uri.to_s
-            end
-
-            expect(yielded).to eq(%w(
-              http://example.com
-              http://example.com?page=2
-              http://example.com?page=3
-              http://example.com?page=4
-            ))
-          end
-        end
-
-        context "with increment step given" do
-          let(:opts) { { param: "page", step: 2 } }
-
-          it "yields the expected URIs" do
-            enum = iterator.to_enum
-            expect(enum.next.to_s).to eq "http://example.com"
-            expect(enum.next.to_s).to eq "http://example.com?page=2"
-            expect(enum.next.to_s).to eq "http://example.com?page=4"
-          end
-        end
       end
     end
 
