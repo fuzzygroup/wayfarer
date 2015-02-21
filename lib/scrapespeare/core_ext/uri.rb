72f5598ae026bb4f5db6adba2a64ffec633bfc98
Dominic Bauer <bauerdominic@gmail.com>
2015-02-21 13:07:16 +0100

    works!

--- lib/scrapespeare/core_ext/uri.rb
+++ lib/scrapespeare/core_ext/uri.rb
@@ -7,4 +7,14 @@ module URI
     Rack::Utils.parse_nested_query(self.query)
   end
 
+  def set_query_param(key, val)
+    self.query = Rack::Utils.build_query(
+      self.parsed_query.merge({ key => val })
+    )
+  end
+
+  def get_query_param(key)
+    self.parsed_query[key]
+  end
+
 end
--- spec/core_ext/uri_ext_spec.rb
+++ spec/core_ext/uri_ext_spec.rb
@@ -2,7 +2,7 @@ require "spec_helpers"
 
 describe URI do
 
-  let(:uri) { URI("http://example.com/?a=b&c=d#e=f") }
+  let(:uri) { URI("http://example.com/?a=b&c=d") }
 
   describe "#parsed_query" do
     it "returns the parsed query string" do
@@ -10,6 +10,36 @@ describe URI do
         "a" => "b",
         "c" => "d"
       })
+    end
+  end
+
+  describe "#set_query_param" do
+    context "with query parameter present" do
+      it "overrides the query parameter's value" do
+        uri.set_query_param "a", "foo"
+        expect(uri.to_s).to eq "http://example.com/?a=foo&c=d"
+      end
+    end
+
+    context "without query parameter present" do
+      it "appends the query parameter" do
+        uri.set_query_param "e", "f"
+        expect(uri.to_s).to eq "http://example.com/?a=b&c=d&e=f"
+      end
+    end
+  end
+
+  describe "#get_query_param" do
+    context "with query parameter present" do
+      it "returns the query parameter's value" do
+        expect(uri.get_query_param("a")).to eq "b"
+      end
+    end
+
+    context "without query parameter present" do
+      it "returns `nil`" do
+        expect(uri.get_query_param("x")).to be nil
+      end
     end
   end
 
