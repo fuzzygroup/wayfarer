0f53dbc78c521aa0b6f573dadd5a5697340a7ed4
Dominic Bauer <bauerdominic@gmail.com>
2015-02-26 17:17:17 +0100

    checkpoint

--- lib/scrapespeare/dom_paginator.rb
+++ lib/scrapespeare/dom_paginator.rb
@@ -1,8 +1,37 @@
 module Scrapespeare
   class DOMPaginator
 
-    describe "#paginate" do
-      
+    def initialize(scraper, matcher_hash)
+      @scraper      = scraper
+      @matcher      = Matcher.new(matcher_hash)
+      @http_adapter = HTTPAdapters::NetHTTPAdapter.new
+      @parser       = Parser
+    end
+
+    def paginate(uri)
+      _, response_body, _ = fetch(uri)
+      doc = parse(response_body)
+    end
+
+    private
+    def fetch(uri)
+      @http_adapter.fetch(uri)
+    end
+
+    def parse(html_str)
+      @parser.parse(html_str)
+    end
+
+    def pagination_element(doc)
+      matched_nodes = @matcher.match(doc)
+
+      if matched_nodes.empty?
+        fail "NO MATCHED NODES"
+      elsif matched_nodes.count > 1
+        fail "MORE THAN ONE MATCHED NODES"
+      else
+        matched_nodes.first
+      end
     end
 
   end
--- spec/dom_paginator_spec.rb
+++ spec/dom_paginator_spec.rb
@@ -3,8 +3,22 @@ require "spec_helpers"
 module Scrapespeare
   describe DOMPaginator do
 
-    def initialize(matcher_hash)
-      @matcher_hash = matcher_hash
+    let(:paginator) { DOMPaginator.new(scraper, matcher_hash) }
+
+    describe "#paginate" do
+      let(:scraper) { Scraper.new.css(:title, "title") }
+      let(:matcher_hash) { { css: "li.next a" } }
+      let(:uri) { URI("http://0.0.0.0:8080/paginate?page=1") }
+
+      it "works" do
+        yielded = []
+
+       paginator.paginate(uri) do |result|
+         yielded << result
+       end
+
+      
+      end
     end
 
   end
