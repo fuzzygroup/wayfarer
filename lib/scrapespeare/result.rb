52ee94fca472c27c3003299f740fd91865948ce0
Dominic Bauer <bauerdominic@gmail.com>
2015-03-03 12:08:50 +0100

    works...

--- lib/scrapespeare/result.rb
+++ lib/scrapespeare/result.rb
@@ -3,7 +3,13 @@ module Scrapespeare
 
     def <<(other)
       self.merge(other) do |key, val_self, val_other|
-        [val_self, val_other]
+        if val_self.is_a? Array
+          val_self.concat(val_other)
+        elsif val_other.is_a? Array
+          val_other.concat(val_self)
+        else
+          [val_self, val_other]
+        end
       end
     end
 
--- spec/result_spec.rb
+++ spec/result_spec.rb
@@ -37,4 +37,63 @@ describe Scrapespeare::Result do
     })
   end
 
+  it "works" do
+    result_a = Result[
+      foo: [
+        {
+          bar: 1,
+          baz: 2,
+          qux: 3
+        },
+        {
+          bar: 4,
+          baz: 5,
+          qux: 6
+        },
+      ]
+    ]
+
+    result_b = Result[
+      foo: [
+        {
+          bar: 7,
+          baz: 8,
+          qux: 9
+        },
+        {
+          bar: 10,
+          baz: 11,
+          qux: 12
+        },
+      ]
+    ]
+
+    final = result_a << result_b
+
+    expect(final).to eq({
+      foo: [
+        {
+          bar: 1,
+          baz: 2,
+          qux: 3
+        },
+        {
+          bar: 4,
+          baz: 5,
+          qux: 6
+        },
+        {
+          bar: 7,
+          baz: 8,
+          qux: 9
+        },
+        {
+          bar: 10,
+          baz: 11,
+          qux: 12
+        }
+      ]
+    })
+  end
+
 end
