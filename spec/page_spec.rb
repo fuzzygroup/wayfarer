d7e70251bd929fe789c553508ceafa3862847653
Dominic Bauer <bauerdominic@gmail.com>
2015-03-04 16:02:02 +0100

    reset

--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -1,5 +1,16 @@
 require "spec_helpers"
 
 describe Scrapespeare::Page do
-  
+
+  subject(:page) do
+    uri = URI("http://0.0.0.0/links.html")
+    Fetcher.new.fetch()
+  end
+
+  describe "#parsed_document" do
+    it "returns a parsed HTML document" do
+
+    end
+  end
+
 end
