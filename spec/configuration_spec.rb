14ab7c985dd6ba06284c39945aafc0908f658c16
Dominic Bauer <bauerdominic@gmail.com>
2015-03-04 12:37:36 +0100

    config

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,40 +1,38 @@
 require "spec_helpers"
 
-module Scrapespeare
-  describe Configuration do
+describe Scrapespeare::Configuration do
 
-    let(:config) { Configuration.new }
+  let(:config) { subject.class.new }
 
-    it "allows setting keys and values" do
-      config.foo = :foo
-      expect(config.foo).to be :foo
-    end
-
-    it "has correct default values set" do
-      expect(config.http_adapter).to be :net_http
-      expect(config.verbose).to be false
-      expect(config.max_http_redirects).to be 3
-      expect(config.sanitize_node_content).to be true
-    end
-
-    it "allows overriding default values" do
-      config.verbose = true
-      expect(config.verbose).to be true
-    end
-
-    describe "#reset!" do
-      it "resets keys and values to defaults" do
-        config.verbose = true
-        config.reset!
-        expect(config.verbose).to be false
-      end
-
-      it "unsets non-default keys" do
-        config.foo = :foo
-        config.reset!
-        expect(config.foo?).to be false
-      end
-    end
-
+  it "allows setting keys and values" do
+    config.foo = :foo
+    expect(config.foo).to be :foo
   end
+
+  it "has correct default values set" do
+    expect(config.http_adapter).to be :net_http
+    expect(config.verbose).to be false
+    expect(config.max_http_redirects).to be 3
+    expect(config.sanitize_node_content).to be true
+  end
+
+  it "allows overriding default values" do
+    config.verbose = true
+    expect(config.verbose).to be true
+  end
+
+  describe "#reset!" do
+    it "resets keys and values to defaults" do
+      config.verbose = true
+      config.reset!
+      expect(config.verbose).to be false
+    end
+
+    it "unsets non-default keys" do
+      config.foo = :foo
+      config.reset!
+      expect(config.foo?).to be false
+    end
+  end
+
 end
