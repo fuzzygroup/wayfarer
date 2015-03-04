ce6f78bdb7151071d5ea5b44dfd8af9d2d62094a
Dominic Bauer <bauerdominic@gmail.com>
2015-03-04 12:46:01 +0100

    works!

--- spec/result_spec.rb
+++ spec/result_spec.rb
@@ -4,162 +4,164 @@ describe Scrapespeare::Result do
 
   let(:result) { subject.class.new }
 
-  it "builds the expected Hash structure" do
-    a = {
-      foo: {
-        bar: 1,
-        baz: 2,
-        qux: 3
-      }
-    }
-
-    b = {
-      foo: {
-        bar: 4,
-        baz: 5,
-        qux: 6
-      }
-    }
-
-    result << a
-    result << b
-
-    expect(result.to_h).to eq({
-      foo: [
-        {
+  describe "#<<" do
+    it "builds the expected Hash structure" do
+      a = {
+        foo: {
           bar: 1,
           baz: 2,
           qux: 3
-        },
-        {
+        }
+      }
+
+      b = {
+        foo: {
           bar: 4,
           baz: 5,
           qux: 6
         }
-      ]
-    })
-  end
-
-  it "builds the expected Hash structure" do
-    a = {
-      foo: [
-        {
-          bar: 1,
-          baz: 2,
-          qux: 3
-        },
-        {
-          bar: 4,
-          baz: 5,
-          qux: 6
-        },
-      ]
-    }
-
-    b = {
-      foo: [
-        {
-          bar: 7,
-          baz: 8,
-          qux: 9
-        },
-        {
-          bar: 10,
-          baz: 11,
-          qux: 12
-        },
-      ]
-    }
-
-    result << a
-    result << b
-
-    expect(result.to_h).to eq({
-      foo: [
-        {
-          bar: 1,
-          baz: 2,
-          qux: 3
-        },
-        {
-          bar: 4,
-          baz: 5,
-          qux: 6
-        },
-        {
-          bar: 7,
-          baz: 8,
-          qux: 9
-        },
-        {
-          bar: 10,
-          baz: 11,
-          qux: 12
-        }
-      ]
-    })
-  end
-
-  it "builds the expected Hash structure" do
-    a = {
-      foo: {
-        bar: [1, 2, 3],
-        baz: { a: 4, b: 5 },
-        qux: 6
       }
-    }
 
-    b = {
-      foo: {
-        bar: 7,
-        baz: 8,
-        qux: 9
-      },
-      qzer: 200
-    }
+      result << a
+      result << b
 
-    result << a
-    result << b
+      expect(result.to_h).to eq({
+        foo: [
+          {
+            bar: 1,
+            baz: 2,
+            qux: 3
+          },
+          {
+            bar: 4,
+            baz: 5,
+            qux: 6
+          }
+        ]
+      })
+    end
 
-    expect(result.to_h).to eq({
-      foo: [
-        {
+    it "builds the expected Hash structure" do
+      a = {
+        foo: [
+          {
+            bar: 1,
+            baz: 2,
+            qux: 3
+          },
+          {
+            bar: 4,
+            baz: 5,
+            qux: 6
+          },
+        ]
+      }
+
+      b = {
+        foo: [
+          {
+            bar: 7,
+            baz: 8,
+            qux: 9
+          },
+          {
+            bar: 10,
+            baz: 11,
+            qux: 12
+          },
+        ]
+      }
+
+      result << a
+      result << b
+
+      expect(result.to_h).to eq({
+        foo: [
+          {
+            bar: 1,
+            baz: 2,
+            qux: 3
+          },
+          {
+            bar: 4,
+            baz: 5,
+            qux: 6
+          },
+          {
+            bar: 7,
+            baz: 8,
+            qux: 9
+          },
+          {
+            bar: 10,
+            baz: 11,
+            qux: 12
+          }
+        ]
+      })
+    end
+
+    it "builds the expected Hash structure" do
+      a = {
+        foo: {
           bar: [1, 2, 3],
           baz: { a: 4, b: 5 },
           qux: 6
-        },
-        {
+        }
+      }
+
+      b = {
+        foo: {
           bar: 7,
           baz: 8,
           qux: 9
-        }
-      ],
-      qzer: 200
-    })
-  end
+        },
+        qzer: 200
+      }
 
-  it "builds the expected Hash structure" do
-    a = {
-      foo: [
-        { x: 1, y: { z: [2, 3, 4] } },
-        { x: 5, y: { z: 6 } },
-      ]
-    }
+      result << a
+      result << b
 
-    b = {
-      foo: { x: [7, 8], y: { z: 9 } }
-    }
+      expect(result.to_h).to eq({
+        foo: [
+          {
+            bar: [1, 2, 3],
+            baz: { a: 4, b: 5 },
+            qux: 6
+          },
+          {
+            bar: 7,
+            baz: 8,
+            qux: 9
+          }
+        ],
+        qzer: 200
+      })
+    end
 
-    result << a
-    result << b
+    it "builds the expected Hash structure" do
+      a = {
+        foo: [
+          { x: 1, y: { z: [2, 3, 4] } },
+          { x: 5, y: { z: 6 } },
+        ]
+      }
 
-    expect(result.to_h).to eq({
-      foo: [
-        { x: 1, y: { z: [2, 3, 4] } },
-        { x: 5, y: { z: 6 } },
-        { x: [7, 8], y: { z: 9 } }
-      ]
-    })
+      b = {
+        foo: { x: [7, 8], y: { z: 9 } }
+      }
+
+      result << a
+      result << b
+
+      expect(result.to_h).to eq({
+        foo: [
+          { x: 1, y: { z: [2, 3, 4] } },
+          { x: 5, y: { z: 6 } },
+          { x: [7, 8], y: { z: 9 } }
+        ]
+      })
+    end
   end
 
 end
