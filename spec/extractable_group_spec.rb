4043b2c77b055b2e7f8362a6f5ac963c460c51cc
Dominic Bauer <bauerdominic@gmail.com>
2015-03-04 12:42:25 +0100

    works!

--- spec/extractable_group_spec.rb
+++ spec/extractable_group_spec.rb
@@ -1,44 +1,42 @@
 require "spec_helpers"
 
-module Scrapespeare
-  describe ExtractableGroup do
+describe Scrapespeare::ExtractableGroup do
 
-    let(:extractable_group) { Scrapespeare::ExtractableGroup.new(:foo) }
+  let(:extractable_group) { ExtractableGroup.new(:foo) }
 
-    describe "#initialize" do
-      it "sets @identifier correctly" do
-        expect(extractable_group.identifier).to be :foo
-      end
+  describe "#initialize" do
+    it "sets @identifier correctly" do
+      expect(extractable_group.identifier).to be :foo
     end
-
-    describe "#extract" do
-      let(:doc) do
-        Nokogiri::HTML <<-html
-          <span id="foo">Foo</span>
-          <span id="bar">Bar</span>
-        html
-      end
-
-      context "without nested Extractables" do
-        it "returns a Hash with an empty String as value" do
-          result = extractable_group.extract(doc)
-          expect(result).to eq({ foo: "" })
-        end
-      end
-
-      context "with nested Extractables" do
-        before do
-          extractable_group.css(:bar, "#bar")
-        end
-
-        it "returns the expected Hash structure" do
-          result = extractable_group.extract(doc)
-          expect(result).to eq({
-            foo: { bar: "Bar" }
-          })
-        end
-      end
-    end
-
   end
+
+  describe "#extract" do
+    let(:doc) do
+      Nokogiri::HTML <<-html
+        <span id="foo">Foo</span>
+        <span id="bar">Bar</span>
+      html
+    end
+
+    context "without nested Extractables" do
+      it "returns a Hash with an empty String as value" do
+        result = extractable_group.extract(doc)
+        expect(result).to eq({ foo: "" })
+      end
+    end
+
+    context "with nested Extractables" do
+      before do
+        extractable_group.css(:bar, "#bar")
+      end
+
+      it "returns the expected Hash structure" do
+        result = extractable_group.extract(doc)
+        expect(result).to eq({
+          foo: { bar: "Bar" }
+        })
+      end
+    end
+  end
+
 end
