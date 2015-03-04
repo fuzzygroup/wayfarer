e571574ff3ce85e18b26d3834dec399642f0b726
Dominic Bauer <bauerdominic@gmail.com>
2015-03-04 12:45:06 +0100

    parser

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,18 +1,16 @@
 require "spec_helpers"
 
-module Scrapespeare
-  describe Parser do
+describe Scrapespeare::Parser do
 
-    let(:parser) { Parser }
+  let(:parser) { subject }
 
-    describe ".parse" do
-      it "returns a parsed HTML document" do
-        html_str = "<span>Foobar</span>"
-        doc = parser.parse(html_str)
+  describe ".parse" do
+    it "returns a parsed HTML document" do
+      html_str = "<span>Foobar</span>"
+      doc = parser.parse(html_str)
 
-        expect(doc).to be_a Nokogiri::HTML::Document
-      end
+      expect(doc).to be_a Nokogiri::HTML::Document
     end
-
   end
+
 end
