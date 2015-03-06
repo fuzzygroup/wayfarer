0c5e2e34708825d478c3c149cacb221072050067
Dominic Bauer <bauerdominic@gmail.com>
2015-03-06 12:42:25 +0100

    werks

--- lib/scrapespeare/routing/router.rb
+++ lib/scrapespeare/routing/router.rb
@@ -2,11 +2,14 @@ module Scrapespeare
   module Routing
     class Router
 
+      attr_reader :routing_table
+
       def initialize
         @routing_table = {}
       end
 
       def register(rule, scraper)
+        @routing_table[rule] = scraper
       end
 
       def route(uri)
--- spec/routing/router_spec.rb
+++ spec/routing/router_spec.rb
@@ -2,7 +2,14 @@ require "spec_helpers"
 
 describe Scrapespeare::Routing::Router do
 
+  subject(:router) { Router.new }
+
   describe "#register" do
+    it "registers a Scraper associated with a Rule" do
+      expect {
+        router.register("/foo", :foobar)
+      }.to change{ router.routing_table.keys.count }.by(1)
+    end
   end
 
 end
