5a6b31b86984f9f242d1fb23142f72ebbb3572cd
Dominic Bauer <bauerdominic@gmail.com>
2015-03-13 12:16:14 +0100

    reset

--- lib/scrapespeare/routing/router.rb
+++ lib/scrapespeare/routing/router.rb
@@ -2,30 +2,6 @@ module Scrapespeare
   module Routing
     class Router
 
-      attr_reader :routing_table
-
-      def initialize
-        @routing_table = {}
-      end
-
-      def register(pattern_str, scraper_sym = :default)
-        @routing_table[Rule.new(pattern_str)] = scraper_sym
-      end
-
-      alias_method :follow, :register
-
-      def recognized?(uri)
-        @routing_table.keys.any? { |rule| rule === uri }
-      end
-
-      def route(uri)
-        @routing_table.each do |rule, scraper_sym|
-          return scraper_sym if rule === uri
-        end
-
-        nil
-      end
-
     end
   end
 end
