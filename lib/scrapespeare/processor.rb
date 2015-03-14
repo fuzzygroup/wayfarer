51f9e94ae77ad6c26baf2ba48c2abf5be971f915
Dominic Bauer <bauerdominic@gmail.com>
2015-03-14 10:51:17 +0100

    checkpoint

--- lib/scrapespeare/processor.rb
+++ lib/scrapespeare/processor.rb
@@ -1,12 +1,28 @@
+require "uri"
+require "thread"
+
 module Scrapespeare
   class Processor
 
-    def initialize(scrapers, router)
-      @router = router
+    def initialize(entry_uri, scraper_table, router)
+      @scraper_table = scraper_table
+      @router        = router
+      @result        = Result.new
+      @fetcher       = Fetcher.new
+      @mutex         = Mutex.new
+
+      @current_uris   = [entry_uri]
+      @staged_uris    = []
+      @processed_uris = []
+    end
+
+    def process
+      uri = next_uri
     end
 
     private
-    def fetch(uri)
+    def next_uri
+      @mutex.synchronize { @current_uris.shift }
     end
 
   end
--- spec/processor_spec.rb
+++ spec/processor_spec.rb
@@ -2,4 +2,23 @@ require "spec_helpers"
 
 describe Scrapespeare::Processor do
 
+  let(:entry_uri) { URI("http://0.0.0.0:9876/graph/index.html") }
+  let(:scraper) { Scraper.new { css :title, ".title" } }
+  let(:scraper_table) { Hash[foo: scraper, catch_all: scraper] }
+  let(:router) do
+    Router.new
+      .register("/foo", :foo)
+      .register("/*catch_all", :catch_all)
+  end
+
+  subject(:processor) { Processor.new(entry_uri, scraper_table, router) }
+
+  describe "#next_uri" do
+    context "with current URIs present" do
+      it "returns the next URI" do
+        
+      end
+    end
+  end
+
 end
