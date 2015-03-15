a998cf85039dc77d1cdd78e7681ea70c23fd8c30
Dominic Bauer <bauerdominic@gmail.com>
2015-03-15 18:53:29 +0100

    works!

--- lib/scrapespeare/routing/router.rb
+++ lib/scrapespeare/routing/router.rb
@@ -2,8 +2,20 @@ module Scrapespeare
   module Routing
     class Router
 
+      attr_reader :whitelist
+      attr_reader :blacklist
+
       def initialize
-        @whitelist = []
+        @whitelist = Rule.new
+        @blacklist = Rule.new
+      end
+
+      def allow(&proc)
+        @whitelist.instance_eval(&proc)
+      end
+
+      def forbid(&proc)
+        @blacklist.instance_eval(&proc)
       end
 
     end
--- spec/routing/router_spec.rb
+++ spec/routing/router_spec.rb
@@ -2,4 +2,22 @@ require "spec_helpers"
 
 describe Scrapespeare::Routing::Router do
 
+  subject(:router) { Router.new }
+
+  describe "#allow" do
+    it "evaluates the given Proc in its `@whitelist`'s instance context" do
+      this = nil
+      router.allow { this = self }
+      expect(this).to be router.whitelist
+    end
+  end
+
+  describe "#forbid" do
+    it "evaluates the given Proc in its `@blacklist`'s instance context" do
+      this = nil
+      router.forbid { this = self }
+      expect(this).to be router.blacklist
+    end
+  end
+
 end
