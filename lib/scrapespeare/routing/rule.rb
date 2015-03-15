62e354253929ddf7d79fa78b9912bf40b9a5c401
Dominic Bauer <bauerdominic@gmail.com>
2015-03-15 19:28:47 +0100

    orks!

--- lib/scrapespeare/routing/rule.rb
+++ lib/scrapespeare/routing/rule.rb
@@ -10,14 +10,13 @@ module Scrapespeare
       end
 
       def matches?(uri)
-        return false unless match(uri)
-        return true if @sub_rules.empty?
+        return match(uri) if @sub_rules.empty?
 
         @sub_rules.reduce(false) { |bool, rule| bool || rule.matches?(uri) }
       end
 
       def match(uri)
-        true
+        false
       end
 
       def host(str_or_regexp, &proc)
