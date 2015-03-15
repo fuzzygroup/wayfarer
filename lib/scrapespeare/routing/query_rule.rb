283ded736f29dcff9850bf1bf34f993b9c579496
Dominic Bauer <bauerdominic@gmail.com>
2015-03-15 12:28:02 +0100

    works!

--- lib/scrapespeare/routing/query_rule.rb
+++ lib/scrapespeare/routing/query_rule.rb
@@ -23,11 +23,16 @@ module Scrapespeare
       def violates_constraint?(constraint, vals)
         case constraint
         when Regexp then violates_regexp?(constraint, vals)
+        when String then violates_string?(constraint, vals)
         end
       end
 
       def violates_regexp?(regexp, vals)
         vals.none? { |val| regexp === val }
+      end
+
+      def violates_string?(str, vals)
+        vals.none? { |val| str == val }
       end
 
     end
--- spec/routing/query_rule_spec.rb
+++ spec/routing/query_rule_spec.rb
@@ -4,6 +4,26 @@ describe Scrapespeare::Routing::QueryRule do
 
   subject(:rule) { QueryRule.new(constraints) }
 
+  describe "String matching constraints" do
+    let(:constraints) { Hash[arg: "foo"]  }
+
+    context "with matching query field value" do
+      let(:uri) { URI("http://example.com?arg=foo") }
+
+      it "returns `true`" do
+        expect(rule.matches?(uri)).to be true
+      end
+    end
+
+    context "with mismatching query field value" do
+      let(:uri) { URI("http://example.com?arg=bar") }
+
+      it "returns `false`" do
+        expect(rule.matches?(uri)).to be false
+      end
+    end
+  end
+
   describe "RegExp constraints" do
     let(:constraints) { Hash[arg: /foo/]  }
 
