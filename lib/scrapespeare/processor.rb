18533acf59ba2e9634269cb1cf1f322dfb98d383
Dominic Bauer <Bauer>
2015-03-21 18:10:51 +0100

    checkpoint

--- lib/scrapespeare/processor.rb
+++ lib/scrapespeare/processor.rb
@@ -4,5 +4,27 @@ require "thread"
 module Scrapespeare
   class Processor
 
+    def initialize(scraper, router)
+      @scraper = scraper
+      @router  = router
+      @fetcher = Fetcher.new
+
+      @current_uris = []
+      @staged_uris  = []
+      @cached_uris  = []
+    end
+
+    def process
+    end
+
+    private
+    def fetch(uri)
+      @fetcher.fetch(uri)
+    end
+
+    def next_uri
+      
+    end
+
   end
 end
--- spec/processor_spec.rb
+++ spec/processor_spec.rb
@@ -1,4 +1,10 @@
 require "spec_helpers"
 
 describe Scrapespeare::Processor do
+
+  describe "#stage_uri" do
+    it "stages an URI for processing" do
+      
+    end
+  end
 end
