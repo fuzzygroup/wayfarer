38f1e49916525a62cf5efa50ff85db02dacabe26
Dominic Bauer <Bauer>
2015-03-23 14:42:35 +0100

    dont return `self`

--- lib/scrapespeare/extraction/extractable.rb
+++ lib/scrapespeare/extraction/extractable.rb
@@ -13,24 +13,20 @@ module Scrapespeare
         extractables << Extractor.new(
           identifier, { css: selector }, *target_attrs, &proc
         )
-        self
       end
 
       def xpath(identifier, expression, *target_attrs, &proc)
         extractables << Extractor.new(
           identifier, { xpath: expression }, *target_attrs, &proc
         )
-        self
       end
 
       def group(identifier, &proc)
         extractables << ExtractableGroup.new(identifier, &proc)
-        self
       end
 
       def scope(matcher_hash, &proc)
         extractables << Scoper.new(matcher_hash, &proc)
-        self
       end
 
     end
