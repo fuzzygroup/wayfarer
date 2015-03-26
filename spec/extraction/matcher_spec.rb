11c319fdb271eca98f1603a694363e584ee4ca16
Dominic Bauer <Bauer>
2015-03-26 19:04:26 +0100

    add tests for matcher

---
+++ spec/extraction/matcher_spec.rb
@@ -0,0 +1,46 @@
+require "spec_helpers"
+
+describe Schablone::Extraction::Matcher do
+
+  let(:doc) do
+    Nokogiri::HTML <<-html
+      <span id="foo">Foo</span>
+      <span id="bar">Bar</span>
+    html
+  end
+
+  subject(:matcher) { Matcher.new(matcher_hash) }
+
+  describe "#initialize" do
+    let(:matcher_hash) { { css: "#foo" } }
+
+    it "sets `@type`" do
+      expect(matcher.type).to be :css
+    end
+
+    it "sets `@type`" do
+      expect(matcher.expression).to eq "#foo"
+    end
+  end
+
+  describe "#match" do
+    context "with CSS selector" do
+      let(:matcher_hash) { { css: "#foo" } }
+
+      it "returns the matched NodeSet" do
+        matched_nodes = matcher.match(doc)
+        expect(matched_nodes.count).to be 1
+      end
+    end
+
+    context "with XPath selector" do
+      let(:matcher_hash) { { xpath: "//*[@id='foo']" } }
+
+      it "returns the matched NodeSet" do
+        matched_nodes = matcher.match(doc)
+        expect(matched_nodes.count).to be 1
+      end
+    end
+  end
+
+end
