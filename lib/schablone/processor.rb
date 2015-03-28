9e1538882132120561edded58a1a8c1f25b97823
Dominic Bauer <Bauer>
2015-03-28 11:24:08 +0100

    works so far

--- lib/schablone/processor.rb
+++ lib/schablone/processor.rb
@@ -4,85 +4,25 @@ require "thread/pool"
 
 module Schablone
   class Processor
+
     attr_reader :current_uris
     attr_reader :staged_uris
-    attr_reader :cached_uris
-    attr_reader :result
+    attr_reader :processed_uris
 
-    def initialize(entry_uri, scraper, router)
-      @scraper = scraper
-      @router  = router
-      @fetcher = Fetcher.new
-
-      @result  = []
-
-      @current_uris = [entry_uri]
-      @staged_uris  = []
-      @cached_uris  = []
-
-      @pool  = Thread.pool(Schablone.config.threads)
-      @mutex = Mutex.new
-    end
-
-    def run
-      @pool.process { step while has_next_uri? }
-      @pool.shutdown
-    end
-
-    def step
-      uri = next_uri
-
-      Schablone.logger.info "About to hit #{uri}"
-
-      cache_uri(uri)
-      page = fetch(uri)
-
-      Schablone.logger.info "Reponse from #{uri}: #{page.status_code}"
-
-      return if page.status_code > 299
-
-      page.links.each { |uri| stage_uri(uri) }
-
-      @result << @scraper.extract(page.parsed_document)
-
-    rescue => error
-      Schablone.logger.fatal(error)
+    def initialize
+      @current_uris = []
+      @staged_uris = []
+      @processed_uris = []
     end
 
     private
-
-    def fetch(uri)
-      @fetcher.fetch(uri)
+    def stage(uri)
+      @staged_uris << uri unless processed?(uri)
     end
 
-    def next_uri
-      @mutex.synchronize { @current_uris.shift }
+    def processed?(uri)
+      @processed_uris.include?(uri)
     end
 
-    def has_next_uri?
-      @mutex.synchronize do
-        return true unless @current_uris.empty?
-        @staged_uris.any? ? (cycle; true) : false
-      end
-    end
-
-    def stage_uri(uri)
-      @mutex.synchronize do
-        return if @current_uris.include?(uri) ||
-                  @staged_uris.include?(uri)  ||
-                  @cached_uris.include?(uri)  ||
-                  @router.forbids?(uri)
-
-        @staged_uris << uri
-      end
-    end
-
-    def cache_uri(uri)
-      @mutex.synchronize { @cached_uris << uri }
-    end
-
-    def cycle
-      @current_uris, @staged_uris = @staged_uris, []
-    end
   end
 end
--- spec/processor_spec.rb
+++ spec/processor_spec.rb
@@ -1,180 +1,61 @@
 require "spec_helpers"
 
 describe Schablone::Processor do
-  let(:entry_uri) { URI("http://0.0.0.0:9876/graph/index.html") }
-  let(:scraper) { Scraper.new { css :title, "title" } }
-  let(:router) do
-    router = Router.new
-    router.allow.host("0.0.0.0")
-    router
-  end
 
-  subject(:processor) { Processor.new(entry_uri, scraper, router) }
+  subject(:processor) { Processor.new }
 
   describe "#initialize" do
-    it "sets the entry URI as current" do
-      expect(processor.current_uris).to eq [entry_uri]
+    it "sets `@current_uris` to an empty list" do
+      expect(processor.current_uris).to eq []
+    end
+
+    it "sets `@staged_uris` to an empty list" do
+      expect(processor.staged_uris).to eq []
+    end
+
+    it "sets `@processed_uris` to an empty list" do
+      expect(processor.processed_uris).to eq []
     end
   end
 
-  describe "#cache_uri" do
-    it "caches the URI" do
+  describe "#stage" do
+    it "stages a URI" do
       uri = URI("http://example.com")
-      expect do
-        processor.send(:cache_uri, uri)
-      end.to change { processor.cached_uris.count }.by(1)
-    end
-  end
-
-  describe "#stage_uri" do
-    context "with URI allowed by Router given" do
-      let(:uri) { URI("http://0.0.0.0/foo") }
-
-      it "stages the URI" do
-        expect do
-          processor.send(:stage_uri, uri)
-        end.to change { processor.staged_uris.count }.by(1)
-      end
-
-      it "does not stage the same URI twice" do
-        processor.send(:stage_uri, uri)
-        processor.send(:stage_uri, uri)
-        expect(processor.staged_uris.count).to be 1
-      end
-
-      it "does not stage current URIs" do
-        processor.instance_variable_set(:@current_uris, [uri])
-        processor.send(:stage_uri, uri)
-        expect(processor.staged_uris).to be_empty
-      end
+      expect {
+        processor.send(:stage, uri)
+      }.to change { processor.staged_uris.count }.by(1)
     end
 
-    context "with URI forbidden by Router given" do
+    context "with already processed URI" do
       let(:uri) { URI("http://example.com") }
+      before { processor.instance_variable_set(:@processed_uris, [uri]) }
 
       it "does not stage the URI" do
-        expect do
-          processor.send(:stage_uri, uri)
-        end.not_to change { processor.staged_uris.count }
-      end
-    end
-
-    context "with already cached URI given" do
-      let(:uri) { URI("http://example.com") }
-      before { processor.send(:cache_uri, uri) }
-
-      it "does not stage the URI" do
-        expect do
-          processor.send(:stage_uri, uri)
-        end.not_to change { processor.staged_uris.count }
+        expect {
+          processor.send(:stage, uri)
+        }.not_to change { processor.staged_uris.count }
       end
     end
   end
 
-  describe "#next_uri" do
-    let(:uri) { URI("http://example.com") }
-    before { processor.instance_variable_set(:@current_uris, [uri]) }
-
-    it "pops the next current URI" do
-      expect(processor.send(:next_uri)).to eq uri
-      expect(processor.current_uris).to be_empty
-    end
-  end
-
-  describe "#has_next_uri?" do
-    context "with current URIs present" do
+  describe "#processed?" do
+    context "with already processed URI" do
       let(:uri) { URI("http://example.com") }
-
-      before do
-        processor.instance_variable_set(:@current_uris, [uri])
-      end
+      before { processor.instance_variable_set(:@processed_uris, [uri]) }
 
       it "returns `true`" do
-        expect(processor.send(:has_next_uri?)).to be true
+        expect(processor.send(:processed?, uri)).to be true
       end
     end
 
-    context "without current URIs present" do
-      before { processor.instance_variable_set(:@current_uris, []) }
+    context "with unprocessed URI" do
+      let(:uri) { URI("http://example.com") }
+      before { processor.instance_variable_set(:@processed_uris, [uri]) }
 
-      context "with staged URIs present" do
-        let(:uri) { URI("http://0.0.0.0/foo") }
-        before { processor.send(:stage_uri, uri) }
-
-        it "returns `true`" do
-          expect(processor.send(:has_next_uri?)).to be true
-        end
-
-        it "cycles" do
-          expect(processor.send(:has_next_uri?)).to be true
-          expect(processor.current_uris).to eq [uri]
-          expect(processor.staged_uris).to be_empty
-        end
-      end
-
-      context "without staged URIS present" do
-        it "returns `false`" do
-          expect(processor.send(:has_next_uri?)).to be false
-        end
+      it "returns `true`" do
+        expect(processor.send(:processed?, uri)).to be true
       end
     end
   end
 
-  describe "#cycle" do
-    let(:uri) { URI("http://0.0.0.0/foo") }
-    before { processor.send(:stage_uri, uri) }
-
-    it "sets the former staged URIs as current" do
-      processor.send(:cycle)
-      expect(processor.current_uris).to eq [uri]
-    end
-
-    it "empties the staged URIs" do
-      processor.send(:cycle)
-      expect(processor.staged_uris).to be_empty
-    end
-  end
-
-  describe "#step" do
-    before { router.allow.host(/.ken/) }
-    before { processor.step }
-
-    it "processes the current URI" do
-      expect(processor.current_uris).to be_empty
-    end
-
-    it "stages the linked URIs" do
-      expected_uris = %w(
-        http://0.0.0.0:9876/graph/details/a.html
-        http://0.0.0.0:9876/graph/details/b.html
-        http://0.0.0.0:9876/status_code/400
-        http://0.0.0.0:9876/status_code/403
-        http://0.0.0.0:9876/status_code/404
-        http://bro.ken
-        http://0.0.0.0:9876/redirect_loop
-      ).map { |str| URI(str) }
-      expect(processor.staged_uris).to eq expected_uris
-    end
-
-    it "caches the processed URI" do
-      expect(processor.cached_uris).to eq [entry_uri]
-    end
-
-    it "appends to `@result`" do
-      expect(processor.result.count).to be 1
-    end
-  end
-
-  describe "#run" do
-    before { router.allow.host(/.ken/) }
-
-    it "works" do
-      processor.run
-      expect(processor.result).to eq [
-        { title: "Index" },
-        { title: "Detail A" },
-        { title: "Detail B" }
-      ]
-    end
-  end
 end
