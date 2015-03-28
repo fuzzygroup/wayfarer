9a1509cee6be0e5a99f0c25bd7bb47650bdf71bc
Dominic Bauer <Bauer>
2015-03-28 22:27:31 +0100

    update bm

--- benchmarks/processor_bm.rb
+++ benchmarks/processor_bm.rb
@@ -10,6 +10,6 @@ haystack = 10000.times.map { URI(Faker::Internet.url) }
 haystack.each { |uri| processor.send(:cache, uri) }
 needle = haystack.sample
 
-Benchmark.bm(5) do |bm|
-  bm.report("#cached?") { foo = processor.send(:cached?, needle); puts foo }
+Benchmark.bm(10) do |bm|
+  bm.report("#cached?") { processor.send(:cached?, needle) }
 end
