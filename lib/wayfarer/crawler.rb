a413124850d09a97e452eb9f280d5b4611c9ef3c
Dominic Bauer <bauerdominic@gmail.com>
2015-10-19 15:56:48 +0200

    document crawler

--- lib/wayfarer/crawler.rb
+++ lib/wayfarer/crawler.rb
@@ -1,7 +1,6 @@
 module Wayfarer
+  # Entry-point for initiating a new crawl
   class Crawler
-    include Celluloid::Internals::Logger
-
     def initialize
       Wayfarer.log.debug("[#{self}] Hello from Wayfarer #{Wayfarer::VERSION}")
 
@@ -9,6 +8,9 @@ module Wayfarer
       Celluloid::Actor[:processor] = Processor.new
     end
 
+    # Stages URIs for the first cycle and runs a {Processor}
+    # @param [Job] klass the job to run.
+    # @param [*Array<URI>, *Array<String>] *uris the URIs to stage for the first cycle.
     def crawl(klass, *uris)
       Wayfarer.log.debug("[#{self}] Staging initial URIs")
       navigator.stage(*uris)
@@ -22,10 +24,14 @@ module Wayfarer
       Wayfarer.log.debug("[#{self}] Done")
     end
 
+    # Returns the spawned {Processor}
+    # @return [Processor]
     def processor
       Celluloid::Actor[:processor]
     end
 
+    # Returns the spawned {Processor}’s {Navigator}
+    # @return [Navigator]
     def navigator
       processor.navigator
     end
