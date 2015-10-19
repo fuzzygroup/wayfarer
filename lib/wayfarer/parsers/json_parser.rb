e55eb74ac8cdb823d64e47056275d17da706a298
Dominic Bauer <bauerdominic@gmail.com>
2015-10-19 15:49:00 +0200

    go on

--- lib/wayfarer/parsers/json_parser.rb
+++ lib/wayfarer/parsers/json_parser.rb
@@ -1,8 +1,12 @@
 module Wayfarer
   module Parsers
+    # A wrapper class for parsing JSON
     module JSONParser
       module_function
 
+      # Parses a JSON string.
+      # @param [String] the JSON string to parse.
+      # @return [OpenStruct]
       def parse(json_str)
         if defined?(Oj)
           Oj.load(json_str)
--- lib/wayfarer/parsers/xml_parser.rb
+++ lib/wayfarer/parsers/xml_parser.rb
@@ -2,13 +2,20 @@ require "nokogiri"
 
 module Wayfarer
   module Parsers
+    # A wrapper class for parsing HTML/XML
     module XMLParser
       module_function
 
+      # Parses an XML string.
+      # @param [String] the XML string to parse.
+      # @return [Nokogiri::XML::Document]
       def parse_xml(xml_str)
         Nokogiri::XML(xml_str)
       end
 
+      # Parses a HTML string.
+      # @param [String] the HTML string to parse.
+      # @return [Nokogiri::HTML::Document]
       def parse_html(html_str)
         Nokogiri::HTML(html_str)
       end
