7fd3e1148d1e0d64616b30c2167dd5aefb6b7729
Dominic Bauer <bauerdominic@gmail.com>
2017-01-08 23:47:47 +0100

    fix locals

--- lib/wayfarer.rb
+++ lib/wayfarer.rb
@@ -36,7 +36,6 @@ require_relative "wayfarer/locals"
 require_relative "wayfarer/job"
 require_relative "wayfarer/finders"
 require_relative "wayfarer/page"
-require_relative "wayfarer/uri_set"
 require_relative "wayfarer/dispatcher"
 require_relative "wayfarer/processor"
 
--- lib/wayfarer/locals.rb
+++ lib/wayfarer/locals.rb
@@ -12,16 +12,27 @@ module Wayfarer
       self.class.locals
     end
 
+    module InstanceMethods
+      def method_missing(method, *argv, &proc)
+        locals.keys.include?(method) ? locals[method] : super
+      end
+
+      def respond_to_missing?(method, private = false)
+        locals.keys.include?(method) || super
+      end
+    end
+
+    include InstanceMethods
+
     module ClassMethods
+      include InstanceMethods
+
       def let(key)
         locals[key] = case val = yield
                       when Array then ThreadSafe::Array.new(val)
                       when Hash  then ThreadSafe::Hash.new(val)
                       else val
                       end
-
-        define_method(key) { locals[key] }
-        define_singleton_method(key) { locals[key] }
       end
 
       def locals
